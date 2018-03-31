9d972d5094621f8f25211c4c117d3cc3fb6a80f6
German Gamboa <germangamboa95@gmail.com>
2018-03-31 01:25:49 -0400

    battle functionality added

---
+++ README.md
@@ -0,0 +1,8 @@
+#Pokemon battle game
+The aim of this project is to practice creating a turn-based fighting game using jQuery.
+The game will be working on the Pokemon API which is an open API that helps beginners. 
+
+##Technologies used
+* jQuery for DOM manipulation. 
+* Pokemon API. 
+
--- main.js
+++ main.js
@@ -4,19 +4,76 @@ $( document ).ready(function() {
 
 
     $('.main-selection').on('click', '.card',  function() {
-        console.log(this);
+        userSelection.card = this;
+        $(userSelection.card).attr("data", 'user');
+        userSelection.health = this.attributes.value.value;
+     
+     
+        $('.title').hide();
+        $('.battle-spot').append(userSelection.card);
+        let opponents = $('.main-selection .card');
+        $('.opponent-selection').append(opponents);
+        $('.opponent-selection').before('<h2 class="choose-opp">Choose your opponent!</h2>');
+        $('.battle-spot').before('<h2 class="chosen">Your chosen pokemon</h2>');
+
+    });
+
+    $('.opponent-selection').on('click', '.card', function() {
+        currentEnemy.card = this;
+        $(currentEnemy.card).attr("data", 'enemy');
+        currentEnemy.health = this.attributes.value.value;
+        $('.battle-spot').append(currentEnemy.card);
+        $('.chosen , .choose-opp').hide();
+
+        $('.battle-spot').before('<h2 class="title">FIGHT!</h2>');
+
+        let opponents = $('.opponent-selection .card');
+        $('.waiting-room').append(opponents).addClass('justify-content-end');
+
+        $('.battle-spot').append('<button class="btn btn-danger">ATTACK!</button>');
+    });
+
+    $('.battle-spot').on('click', '.btn', function() {
+        attack();
+        addPower();
+       $('[data="enemy"] .card-text .health').text(currentEnemy.health);
+       $('[data="user"] .card-text .health').text(userSelection.health);
+       $('[data="enemy"] .card-text .attack').text(currentEnemy.base_power);
+       $('[data="user"] .card-text .attack').text(userSelection.curent_power);
+        console.log(userSelection);
+        console.log(currentEnemy);
+
+
     });
 
 
 });
 
 
+
 const userSelection = {
     card: '',
     health: '',
-    multiplier: 
+    multiplier: 10,
+    base_power: 20,
+    curent_power: 10 
 };
 
+const currentEnemy = {
+    card: '',
+    health: '',
+    multiplier: 0,
+    base_power: 10
+}
+function addPower() {
+    userSelection.curent_power += userSelection.multiplier;
+}
+
+function attack() {
+    currentEnemy.health -= userSelection.curent_power;
+    userSelection.health -= currentEnemy.base_power;
+
+}
 
 function createCards(num) {
 
@@ -26,14 +83,16 @@ function createCards(num) {
             .then(res => res.json())
             .then(data => {
                 let image = data.sprites.front_default;
-                let html = `<div value="${data.stats[0].base_stat}" class="card col-md-3" style="width: 18rem;">
+                let html = `<div value="${data.stats[0].base_stat}" class="card col-md-3" >
                 <img class="card-img-top" src="${image}" alt="Card image cap">
                 <div class="card-body">
-                  <p class="card-text">Health: ${data.stats[0].base_stat}</p>
+                  <p class="card-text">Health: <span class="health">${data.stats[0].base_stat}</span></p>
+                  <p class="card-text">Attack: <span class="attack">10</span></p>
+                  
                 </div>
               </div>`;
               html = $(html);
-              console.log(html);
+
                 $('.main-selection').append(html);
             });
     }
